9dd6a868b85cf4e8344d474e4918d7d3c31fff39
Przemek Grochowski <sovvly@protonmail.com>
2019-12-18 22:52:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/CreateMessageActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+
+class CreateMessageActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+    fun onSendMessage(view: View){
+
+        
+    }
+}
